9079266c94d21b09970a9c25f9c9b0b695dcaf09
XZANATOL <abdelrahmanbedox@gmail.com>
2026-01-30 08:35:46 +0200

    Backend: fixed static files serve using whitenoise | Bug fixes

--- hos-eld-trip-planner/hos_eld_trip_planner/hos_eld_trip_planner/settings.py
+++ hos-eld-trip-planner/hos_eld_trip_planner/hos_eld_trip_planner/settings.py
@@ -41,8 +41,8 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # "django.contrib.admin",
-    # "django.contrib.auth",
-    # "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
     # "django.contrib.sessions",
     # "django.contrib.messages",
     "django.contrib.staticfiles",
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     # "django.contrib.auth.middleware.AuthenticationMiddleware",
     # "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "hos_eld_trip_planner.urls"
@@ -137,3 +139,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
